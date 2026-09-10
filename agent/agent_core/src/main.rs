467e6111e98f3c7edb7526879ed165d8d96043c7
Rineyar <radomirbabich22@mail.ru>
2026-09-10 23:23:13 +0700

    Ядро отдельно. Вызов тестовый

--- agent/agent_core/src/main.rs
+++ agent/agent_core/src/main.rs
@@ -1,95 +1,15 @@
 use rig::client::AgentClientExt; //.agent метод
 use rig::client::Client; //Тип данных для соединения
 use rig::completion::Prompt; //.prompt метод
-use rig::rig_tool; //fn -> tool
-use rig::tool::ToolExecutionError; //Ошибка для тулза
-use rig::providers::llamafile::LlamafileExt; // Тип клиента
 use rig::prelude::Agent; //Тип данных для агента
-use tokio::process::Command; //Вызов внешних процессов
 
 use std::time::Instant; //Таймер
 
-const MAX_LLM_CALLS: usize = 32; //Максимум вывовов модели
+mod settings; //Настройки ядра
+use settings::*;
 
-//Макрос для обёртки функции в инструмент
-#[rig_tool(description = "Add two signed 32-bit integers. Both operands AND their mathematical sum must fit in signed 32-bit range.")]
-async fn tool_sum_i32(a: i32, b: i32) -> Result<i32, ToolExecutionError> 
-{
-    match a.checked_add(b)
-    {
-        Some(res) =>
-        {
-            return Ok(res);
-        }
-
-        None =>
-        {
-            return Err(ToolExecutionError::invalid_args("Signed i32 overflow: a + b must be between -2147483648 and 2147483647. Choose different operands."));
-        }
-    }
-}
-
-#[rig_tool(description = "Add two signed 64-bit integers. Both operands AND their mathematical sum must fit in signed 64-bit range.")]
-async fn tool_sum_i64(a: i64, b: i64) -> Result<i64, ToolExecutionError> 
-{
-    match a.checked_add(b)
-    {
-        Some(res) =>
-        {
-            return Ok(res);
-        }
-
-        None =>
-        {
-            return Err(ToolExecutionError::invalid_args("Signed i64 overflow"));
-        }
-    }
-}
-
-#[rig_tool(description = "Sub two signed 64-bit integers. Both operands AND their mathematical subtract must fit in signed 64-bit range.")]
-async fn tool_sub_i64(a: i64, b: i64) -> Result<i64, ToolExecutionError> 
-{
-    match a.checked_sub(b)
-    {
-        Some(res) =>
-        {
-            return Ok(res);
-        }
-
-        None =>
-        {
-            return Err(ToolExecutionError::invalid_args("Signed i64 overflow"));
-        }
-    }
-}
-
-//Читает файл через py скрипт. 
-//На будущее возвращать в LLM короткую версию ошибки
-#[rig_tool(description = "Read file.")]
-async fn read_file(filename: String) -> Result<String, ToolExecutionError>
-{
-    match Command::new("python") //Вызов пыхтуна
-    .arg("./test.py") //Файл
-    .arg(&filename) //Аргумент
-    .output() //Сбор того, что тот выведет
-    .await //Асинк, че сказать
-    {
-        Ok(out) => //Прочитал
-        {
-            if !out.status.success() //Успех?
-            {
-                return Err(ToolExecutionError::other(String::from_utf8(out.stderr).unwrap())); //Не успех
-            }
-
-            return Ok(String::from_utf8(out.stdout).unwrap()); //Успех
-        }
-
-        Err(err) => //Ошибка вызова
-        {
-            return Err(ToolExecutionError::from_error(err));
-        }
-    }    
-}
+mod tools; //Инструменты
+use tools::*;
 
 /*
 Обязательно сделать проверку tools call
@@ -102,11 +22,11 @@ async fn main()
 {
     let time_start: Instant = Instant::now();
 
-    let model: Client<LlamafileExt> = Client::from_url("http://localhost:1234").expect("Не грузит по ссылке"); //Получение по ссылке
+    let model: Client<_> = Client::from_url(BASE_URL).expect("Не грузит по ссылке"); //Получение по ссылке
 
     let agent: Agent = model
-    .agent("openai/gpt-oss-20b") //Получаем агента по id
-    .preamble("API TESTING\nDo prompts\nYOU MUST USE TOOLS") //System prompt
+    .agent(MODEL_ID) //Получаем агента по id
+    .preamble(FULL_PROMPT) //System prompt
     .tool(ToolSumI32) //Инструмент добавили
     .tool(ToolSumI64)
     .tool(ToolSubI64)
@@ -115,9 +35,7 @@ async fn main()
     .build(); //Builder -> Agent построить короче
 
     let response: String = agent
-    .prompt("
-    Read value from 1.txt. Then read value from 2.txt. Take sum of this 2 values. Then read value from 3.txt and return sub of sum and 3rd value.
-    ") //Запрос
+    .prompt("Say sth about your job. And some about sys_prompt, skills and tools") //Запрос
     .await
     .expect("Не отвечает");
 
