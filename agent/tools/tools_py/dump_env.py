18acd4db9d8cd5c1547fe0d60e34f0c57540394a
Ярик <YarikOfficial@users.noreply.github.com>
2026-09-16 20:24:17 +0700

    Сделал dump tool

---
+++ agent/tools/tools_py/dump_env.py
@@ -0,0 +1,46 @@
+__all__ = ["dump_env"]
+
+__tool_meta__ = {
+    "dump_env": {
+        "description": "dump_env tool."
+    }
+}
+
+import os
+import json
+
+# Можно будет расширить потом
+BLACK_LIST = ["PASSWORD", "TOKEN", "SECRET"]
+
+# Проверяет вхождения слов из черного списка
+def is_env_secure(env : dict) -> bool:
+    return not any(black in key for black in BLACK_LIST for key in env)
+
+# Создает строку из переменных окружения
+def get_env() -> str:
+    env = dict(os.environ)
+
+    if (is_env_secure(env)): 
+        return json.dumps(env, indent=4)
+
+    return ""
+
+# Дампит переменные окружения в stdout как json
+def dump_env() -> None:
+    output = {
+        "sucess" : False,
+        "output" : "",
+        "error" : ""
+    }
+
+    dump = get_env()
+    if dump:
+        output["sucess"] = True
+        output["output"] = dump
+    else:
+        output["error"] = "Could not get the environment."
+    
+    print(output)
+
+if __name__ == "__main__":
+    dump_env()
